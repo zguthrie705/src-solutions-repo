ba63e2cfdffc1e4e035603fcf9e6ecfe174be997
undefined <zach.guthrie@slalom.com>
2019-04-30 19:16:05 -0400

    Added error logging and fixed environment variable script.

--- app.js
+++ app.js
@@ -25,66 +25,62 @@ const app = express();
 
 // Creates API connection for private solution repo and gets authUser information.
 const authOctokit = new Octokit({
-  auth: process.env.GITHUB_OAUTH_TOKEN
+    auth: process.env.GITHUB_OAUTH_TOKEN
 });
 
 authOctokit.users.getAuthenticated().then(({data, headers, status}) => {authUser = data;});
 
 // Gets challenge repo and searches for user branch
-async function getRepoAndUserBranch() {
-  let recruitRepo = await authOctokit.repos.get({owner: authUser.login, repo: process.env.SOLUTIONS_REPO_NAME})
-      .then(({data}) => {return data;})
-      .catch(error => {console.log(error)});
+async function getRepoAndUserBranch(next) {
+    let recruitRepo = await authOctokit.repos.get({owner: authUser.login, repo: process.env.SOLUTIONS_REPO_NAME})
+        .then(({data}) => {return data;})
+        .catch(error => {console.log(error)});
 
-  return await authOctokit.repos.getBranch({owner: authUser.login, repo: recruitRepo.name, branch: branchName})
-      .then(({data}) => {
-        return data;
-      })
-      .catch(error => {
-        if (error.status === 404) {
-          return null;
-        } else {
-          console.log('Error code ' + error.status + ': ' + error.stack);
-        }
+    return await authOctokit.repos.getBranch({owner: authUser.login, repo: recruitRepo.name, branch: branchName})
+        .then(({data}) => {
+            return data;
+        })
+        .catch(error => {
+            if (error.status === 404) {
+                return null;
+            } else {
+                next(error);
+            }
 
-      });
+        });
 }
 
-async function createUserBranch() {
-  try {
-    await simpleGit.checkoutBranch(branchName, 'HEAD')
-        .push('origin', branchName, {'--set-upstream': null})
-        .checkout('master')
-        .deleteLocalBranch(branchName);
-  } catch (e) {
-    console.log(e);
-  }
+async function createUserBranch(next) {
+    try {
+        await simpleGit.checkoutBranch(branchName, 'HEAD')
+            .push('origin', branchName, {'--set-upstream': null})
+            .checkout('master')
+            .deleteLocalBranch(branchName);
+    } catch (e) {
+        next(e);
+    }
 }
 
 // Configure the GitHub Strategy for use by Passport
 passport.use(new GitHubStrategy({
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: '/login/github/callback'
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: '/login/github/callback'
     },
     function(accessToken, refreshToken, profile, cb) {
-      currentUser = profile;
-      app.locals.username = currentUser.username;
-      branchName = currentUser.username + "-solution";
-      return cb(null, profile);
+        currentUser = profile;
+        app.locals.username = currentUser.username;
+        branchName = currentUser.username + "-solution";
+        return cb(null, profile);
     }));
-
-// Initialize Passport and restore authentication state, if any, from the session
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Configure Passport authenticated session persistence
 passport.serializeUser(function(user, cb) {
-  cb(null, user);
+    cb(null, user);
 });
 
 passport.deserializeUser(function(user, cb) {
-  cb(null, user);
+    cb(null, user);
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -100,6 +96,71 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
 
+// Initialize Passport and restore authentication state, if any, from the session
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/',
+    ensureLoggedIn(),
+    (req, res, next) => {
+        app.locals.rendFile = 'challenge-setup';
+        getRepoAndUserBranch(next).then((data) => {
+            data !== null ? res.redirect('/challenge-1') : res.render('challenge-setup', {error: req.query.error})
+        });
+    });
+
+app.get('/login', function(req, res, next) {
+    try {
+        res.render('login', {error: req.query.error});
+    } catch(e) {
+        next(e);
+    }
+});
+
+app.get('/challenge-1',
+    ensureLoggedIn(),
+    (req, res, next) => {
+        app.locals.rendFile = 'challenge-1';
+        try {
+            res.render('challenge-1', {error: req.query.error});
+        } catch(e) {
+            next(e);
+        }
+    });
+
+app.get('/create-branch',
+    ensureLoggedIn(),
+    (req, res, next) => {
+        createUserBranch(next).then(() => {
+            try {
+                res.redirect('/');
+            } catch(e) {
+              next(e);
+            }
+        }).catch(e => {next(e)});
+    });
+
+app.get('/login/github', passport.authenticate('github'));
+
+app.get('/login/github/callback', passport.authenticate('github', {failureRedirect: '/login'}),
+    function(req, res, next) {
+        // Successfully authenticated, return home
+        try {
+            res.redirect('/');
+        } catch(e) {
+            next(e);
+        }
+    });
+
+app.get('/logout', function(req, res, next) {
+    try {
+        req.logout();
+        res.redirect('/');
+    } catch(e) {
+        next(e);
+    }
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -114,47 +175,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-app.get('/',
-    ensureLoggedIn(),
-    (req, res) => {
-      app.locals.rendFile = 'challenge-setup';
-      getRepoAndUserBranch().then((data) => {
-        data !== null ? res.redirect('/challenge-1') : res.render('challenge-setup', {error: req.query.error})
-      });
-    });
-
-app.get('/login', function(req, res) {
-  res.render('login', {error: req.query.error});
-});
-
-app.get('/challenge-1',
-    ensureLoggedIn(),
-    (req, res) => {
-      app.locals.rendFile = 'challenge-1';
-      res.render('challenge-1', {error: req.query.error});
-    });
-
-app.get('/create-branch',
-    ensureLoggedIn(),
-    (req, res) => {
-      createUserBranch().then(() => {
-        res.redirect('/');
-      });
-    });
-
-app.get('/login/github', passport.authenticate('github'));
-
-app.get('/login/github/callback', passport.authenticate('github', {failureRedirect: '/login'}),
-    function(req, res) {
-      // Successfully authenticated, return home
-      res.redirect('/');
-    });
-
-app.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('/');
 });
 
 module.exports = app;
